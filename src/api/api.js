9d5c6acfa5ce526e179402ce017141ccd4e72a1e
gebmecod <jybravo@up.edu.ph>
2023-07-03 20:20:08 +0800

    api changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // const baseURL = "http://54.169.72.229:8800";
-const baseURL = process.env.NODE_ENV === "production"
-    ? "https://hyperdevs-4f4d05d29e65.herokuapp.com"
-    : "http://localhost:8800";
+// const baseURL = process.env.NODE_ENV === "production"
+//     ? "https://hyperdevs-4f4d05d29e65.herokuapp.com/"
+//     : "http://localhost:8800";
   // process.env.NODE_ENV === "production"
   //   ? "http://54.169.72.229:8800"
   //   : "http://localhost:8800";
 
 export default axios.create({
-  baseURL,
+  baseURL: "https://hyperdevs-4f4d05d29e65.herokuapp.com",
 });
