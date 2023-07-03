cf4dc734ca31b261d0198cc4d94cb414685c2e64
gebmecod <jybravo@up.edu.ph>
2023-07-03 19:47:25 +0800

    changes in api link

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = "http://54.169.72.229:8800";
 const baseURL = process.env.NODE_ENV === "production"
-    ? "http://54.169.72.229:8800"
+    ? "https://hyperdevs-4f4d05d29e65.herokuapp.com"
     : "http://localhost:8800";
   // process.env.NODE_ENV === "production"
   //   ? "http://54.169.72.229:8800"
